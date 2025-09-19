671ce4de0b0ad65c59a2ad9f1c59c9c25f1e6b15
S-Erik <schultheis.e@freenet.de>
2025-09-19 08:21:25 +0200

    Bugfix: Non-equivalent minima were falsely identified as equivalent when relative difference was small

--- excitationsolve/info.py
+++ excitationsolve/info.py
@@ -1,3 +1,3 @@
-__version__ = "1.1.0"  # MAJOR.MINOR.PATCH, like in https://semver.org/
+__version__ = "1.1.1"  # MAJOR.MINOR.PATCH, like in https://semver.org/
 
 HOMEPAGE = "https://github.com/dlr-wf/ExcitationSolve"  # GitHub page
