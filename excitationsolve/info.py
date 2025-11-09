b9b82d054f64744ff012d0de2e6cdb1c050e7009
S-Erik <schultheis.e@freenet.de>
2025-11-09 17:06:28 +0100

    Removing version requirements for numpy and scipy

--- excitationsolve/info.py
+++ excitationsolve/info.py
@@ -1,3 +1,3 @@
-__version__ = "2.1.1"  # MAJOR.MINOR.PATCH, like in https://semver.org/
+__version__ = "2.1.2"  # MAJOR.MINOR.PATCH, like in https://semver.org/
 
 HOMEPAGE = "https://github.com/dlr-wf/ExcitationSolve"  # GitHub page
